f8900dfa83c9d6fd33d05a0d6fd142eb3738c686
pbdanny <pb.danny@gmail.com>
2017-02-13 12:31:06 +0700

    Final

--- KTC Telesales Web Lead Time Call.R
+++ KTC Telesales Web Lead Time Call.R
@@ -1,16 +1,11 @@
 # Read data -----
-library(readxl)
-web <- read_excel(path = "/Users/Danny/Documents/R Project/KTC.Tele.Web.Time/List Web DateTime.xlsx", 
-                  sheet = 1, col_types = rep("text",7))
-colnames(web) <- make.names(colnames(web))
+# library(readxl)
+# web <- read_excel(path = "/Users/Danny/Documents/R Project/KTC.Tele.Web.Time/List Web DateTime.xlsx", 
+#                  sheet = 1, col_types = rep("text",7))
+# colnames(web) <- make.names(colnames(web))
 
-save(web, file = "TeleWebCallTime.RData")
+# save(web, file = "TeleWebCallTime.RData")
 load(file = "TeleWebCallTime.RData")
-
-View(head(web))
-str(web)
-summary(web)
-View(web[is.na(web$Identifier.ID), ])
 
 # Manipulate date time data ----
 
@@ -33,7 +28,7 @@ web$DateCall <- NULL
 web$TimeCall <- NULL
 
 # Convert all Excel date-time to POSIXct
-web$DateTimeSubmit <- textWinDateConv(web$DateTimeSubmit)
+web$DateTimeSubmit <- textWinDateConv(web$DateTimeApply)
 web$DateTimeExport <- textWinDateConv(web$DateTimeExport)
 web$DateTimeCall <- textWinDateConv(web$DateTimeCall)
 
@@ -48,13 +43,13 @@ web$DateTimeExport <- as.POSIXct(web$DateTimeExport)
 
 # Function find next working day
 
-load(file = "BankSpecialHoliday.RData")
+load(file = "Thai Bank Holiday.RData")
 
 nextWorkDay <- function (d, EndHr, Holiday) {
   out <- list()
   for (i in 1:length(d)) {
-    while (as.integer(format(d[i], "%H")) >= EndHr | format(d[i], "%w") %in% c("0", "6") | 
-           format(d[i], "%Y-%m-%d") %in% Holiday) { 
+    while (as.integer(format(d[i], "%H")) >= EndHr | (format(d[i], "%w") %in% c("0", "6")) | 
+           (format(d[i], "%Y-%m-%d") %in% Holiday)) { 
       d[i] <- d[i] + (24*60*60)  # Add 1 day (= add 24*60*60 seconds)
       d[i] <- as.POSIXct(paste0(format(d[i], "%Y-%m-%d"), " 08:00:00"))  # Move to start working hours
     }
@@ -64,44 +59,93 @@ nextWorkDay <- function (d, EndHr, Holiday) {
 }
 
 # Call function nextWorkDay
-web$LeadLoaded <- nextWorkDay(web$DateTimeSubmit, 16, thaiBankHoliday)
+web$DateTimeStart <- nextWorkDay(web$DateTimeSubmit, 16, thaiBankHoliday)
 
 # Convert text to POSIXct
-web$LeadLoaded <- as.POSIXct(web$LeadLoaded, 
+web$DateTimeStart <- as.POSIXct(web$DateTimeStart, 
                               origin = (as.POSIXct("1899-12-30", tz = "Asia/Bangkok") - (17*60+56)),
                               tz = "Asia/Bangkok")
-# Data analysis -----
-web$hrs.diff <- as.numeric(difftime(web$DateTimeCall, web$LeadLoaded, units ="hours"))
+# Data Audit -----
+web$hrs.diff <- as.numeric(difftime(web$DateTimeCall, web$DateTimeStart, units ="hours"))
 
-# Plot all time called 
+# Correct hrs.diff < 0 caused by DateTimeCall before DateTimeStart caused by leadSubmit after 16:00
+# Then change DateTimeStart back to DateTimeSubmit for hrs.diff < 0
+rep.idx <- which(web$hrs.diff < 0)
+web[rep.idx, "DateTimeStart"] <- web[rep.idx, "DateTimeSubmit"]
+web$hrs.diff <- as.numeric(difftime(web$DateTimeCall, web$DateTimeStart, units ="hours"))
+
+# Select only feasible data
+df <- web[web$hrs.diff > 0 & !is.na(web$hrs.diff) & web$hrs.diff < 168, ]  # Choose hrs.diff < 168 hr(=7 days)
+rm(rep.idx)
+
+# Export Data for error check
+df.err <- web[!(web$Identifier.ID %in% df$Identifier.ID), ]
+library(xlsx)
+write.xlsx2(df.err$Identifier.ID, file = "Error.xlsx", row.names = FALSE)
+rm(df.err)
+
+# Exploratory Data Analysis ----
+# save(df, file = "Tele Web Call Time-Audited.RData")
+
+load(file = "Tele Web Call Time-Audited.RData")
+
+# Basict Stat Part
+
+statsummary.df <- function(df) {
+  sample.mean <- mean(df$hrs.diff)
+  sample.sd <- sd(df$hrs.diff)
+  sample.median <- median(df$hrs.diff)
+  n <- nrow(df)
+  err <- qt(0.975, df = n - 1)*sample.sd/sqrt(n)
+  lower <- sample.mean - err
+  upper <- sample.mean + err
+  out <- data.frame(c("Sample.mean" = sample.mean,
+                      "Sample.SD" = sample.sd,
+                      "95% lower conf. interval" = lower,
+                      "95% upper conf. interval" = upper,
+                      "Median" = sample.median))
+  colnames(out) <- "Hrs.diff"
+  return(out)
+}
+
 library(ggplot2)
-library(plotly)
-g <- ggplot(web[web$hrs.diff > 0, ], aes(x = hrs.diff)) + 
+g <- ggplot(df, aes(x = hrs.diff)) + 
   geom_histogram(binwidth = 1, fill = "green", color = "green", alpha = .5) +
   coord_cartesian(xlim = c(0, 50)) +
-  geom_vline(xintercept = 2, color = "red")
-ggplotly(g)
+  geom_vline(xintercept = 2, color = "red") +
+  geom_vline(xintercept = 5, color = "green", linetype = 2) +
+  ggtitle("Total Hrs. Diff : Time Call - Time Apply") +
+  xlab("Time Call - Time Apply (hrs)")
+g
 
 # add flag holiday
-web$submitHoliday <- ifelse(format(web$DateTimeSubmit, "%Y-%m-%d") %in% thaiBankHoliday |
-                             format(web$DateTimeSubmit, "%w") %in% c("0", "6"), "Holiday", "Not Holiday")
+load(file = "Thai Bank Holiday.RData")
+df$submitHoliday <- ifelse(format(df$DateTimeSubmit, "%Y-%m-%d") %in% thaiBankHoliday |
+                             format(df$DateTimeSubmit, "%w") %in% c("0", "6"), "Holiday", "Not Holiday")
+# Plot Holiday
 
-h <- ggplot(web[web$hrs.diff > 0, ], aes(x = hrs.diff, fill = as.factor(submitHoliday))) + 
+g <- ggplot(df, aes(x = hrs.diff, fill = as.factor(submitHoliday), color = as.factor(submitHoliday))) + 
   geom_histogram(binwidth = 1, alpha = .5, position = "identity") +
   coord_cartesian(xlim = c(0, 50)) +
-  geom_vline(xintercept = 2, color = "red")
-ggplotly(h)
+  ggtitle("Hrs Diff : Holiday vs. Not Holiday") +
+  xlab("Time Call - Time Apply (hrs)")
+g
 
-t.test(hrs.diff ~ submitHoliday, data = web[web$hrs.diff > 0, ])
+stat.holi <- statsummary.df(df[df$submitHoliday == "Holiday", ])
+stat.notholi <- statsummary.df(df[df$submitHoliday == "Not Holiday",])
+t.test(hrs.diff ~ submitHoliday, data = df)
 # T-test show diffrence mean lead submit holiday ~ holiday
 
-web$submitDayofWeek <- as.integer(format(web$DateTimeSubmit, "%w"))
-web$DayofWeek<- factor(web$submitDayofWeek, 
+df$submitDayofWeek <- as.integer(format(df$DateTimeSubmit, "%w"))
+df$DayofWeek<- factor(df$submitDayofWeek, 
                           labels = c("Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"))
 
-i <- ggplot(web[web$hrs.diff > 0, ], aes(x = hrs.diff, color = DayofWeek)) + 
+i <- ggplot(df, aes(x = hrs.diff, color = DayofWeek)) + 
   geom_density(alpha = .25) +
   coord_cartesian(xlim = c(0, 50)) +
   geom_vline(xintercept = 2, color = "red")
-ggplotly(i)
+i
 # Geom desity show lead Sat & Sun has higher means
+
+# Export data for recheck
+write.csv(df, file = "write.csv")
